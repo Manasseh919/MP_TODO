8aa16a99953ae4be3377eb9faa0581fc4f057bca
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2024-01-05 10:18:43 +0000

    endpoint to register user to database

--- api/index.js
+++ api/index.js
@@ -1,24 +1,55 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const crypto = require("crypto")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const app = express();
+const port = 3000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect("mongodb+srv://manasseh919:seyrammann@cluster0.sqmuurg.mongodb.net/")
+  .then(() => {
+    console.log(`Connected to mongoDB ${mongoose.connection.host}`);
+  })
+  .catch((error) => {
+    console.log("error connecting to mongo DB");
+  });
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
+const User = require("./models/user");
+const Todo = require("./models/todo");
 
 
-const app = express()
-const port = 3000
-const cors = require("cors")
-app.use(cors())
+//endpoint to register user to database
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+    ////check if email is already registered
+    const existingUser = await User.findOne({ email });
 
+    if (existingUser) {
+      console.log("email already registered");
+    }
 
-mongoose.connect("mongodb+srv://manasseh919:seyrammann@cluster0.sqmuurg.mongodb.net/").then(()=>{
-    console.log(`Connected to mongoDB ${mongoose.connection.host}`)
-}).catch((error)=>{
-    console.log("error connecting to mongo DB")
-})
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+    await newUser.save();
+    res.status(202).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log("error registering the user", error);
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
